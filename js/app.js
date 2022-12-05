e51c0f1b60edb06b3dba28751aed3df677c280cc
George Ershov <geo.erh96@gmail.com>
2022-12-05 11:29:10 +0300

    add all files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import { scriptInit } from './script.js'
+import { radioPlayerInit } from "./radioPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+
+
+
+scriptInit();
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
