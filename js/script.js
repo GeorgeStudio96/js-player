8c6a025d9dbb45a67fbd3fe79fc7c1f24e2d3b38
George Ershov <geo.erh96@gmail.com>
2022-12-05 11:27:24 +0300

    add all files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log('first')
+export const scriptInit = () => {
+    console.log("Create Functional")
+
+    const tabBtn = document.querySelectorAll('.player-btn');
+    const tabContent = document.querySelectorAll('.player-block')
+
+
+
+    const deactivationPlayer = () => {
+        tabBtn.forEach(function (currentTab) {
+            currentTab.classList.remove('active')
+        })
+        tabContent.forEach(function (tabItem) {
+            tabItem.classList.remove('active')
+        })
+    }
+
+
+    tabBtn.forEach(function (itemTab, index) {
+        itemTab.addEventListener('click', function () {
+            /*tabBtn.forEach((currentTab) => {
+                currentTab.classList.remove('active')
+            }) */
+            deactivationPlayer();
+            tabBtn[index].classList.add('active')
+
+            /*tabContent.forEach(function (tabItem) {
+                tabItem.classList.remove('active')
+            }) */
+
+            tabContent[index].classList.add('active')
+
+
+        })
+
+
+    })
+
+}
